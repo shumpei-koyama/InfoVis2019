6b74f749328b4559c75dc6d016cb065c164c13d6
小山　峻平 <mwaaaa15@gmail.com>
2019-04-17 13:20:11 +0900

    2019/4/17

---
+++ W03/main2.js
@@ -0,0 +1,43 @@
+function main()
+{
+    var width = 500;
+    var height = 500;
+
+    var scene = new THREE.Scene();
+
+    var fov = 45;
+    var aspect = width / height;
+    var near = 1;
+    var far = 1000;
+    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.set( 0, 0, 5 );
+    scene.add( camera );
+    
+   
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width, height );
+    document.body.appendChild( renderer.domElement );
+
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    // var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+     var material = new THREE.MeshLambertMaterial({
+	color: 0xb7ee45
+    });
+    var cube = new THREE.Mesh( geometry, material );
+    var light = new THREE.PointLight(0x4644ac,8,0);
+    light.position.set(1,1,1.5);
+    scene.add(light);
+    
+    scene.add( cube );
+
+    loop();
+
+    function loop()
+    {
+        requestAnimationFrame( loop );
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render( scene, camera );
+    }
+}
